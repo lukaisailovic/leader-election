ca474ab9b545a9c2c1b28079fd68829b4dd41135
lukaisailovic <lukaisailovic@gmail.com>
2022-06-06 20:37:40 +0200

    Ensure that node /votes is always present

--- src/domaci2/LeaderElection.java
+++ src/domaci2/LeaderElection.java
@@ -3,19 +3,33 @@ package domaci2;
 import org.apache.zookeeper.*;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Random;
 
 public class LeaderElection implements Watcher {
+    String connectionString ;
+    DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 
-    String connectionString = "localhost:2181";
-    byte [] serverId = Integer.toHexString(new Random().nextInt()).getBytes();
     ZooKeeper zk;
     boolean isLeader = false;
+    String id;
+
+    public LeaderElection(int id, String connectionString) {
+        Random r = new Random();
+        this.id = ((1 + r.nextInt(2)) * 10000 + r.nextInt(10000)) + "_" + id;
+        this.connectionString = connectionString;
+    }
+
+    private byte[] getIdBytes() {
+        return this.id.getBytes();
+    }
 
     @Override
     public void process(WatchedEvent event) {
-        System.out.println("NEW EVENT "+event.getPath());
-        if (event.getPath().equals("/leader")){
+        System.out.println("NEW EVENT " + event.getPath());
+        if (event.getPath().equals("/leader")) {
             try {
                 join();
             } catch (Exception e) {
@@ -23,11 +37,12 @@ public class LeaderElection implements Watcher {
             }
         }
     }
+
     public void start() throws IOException {
-        this.zk = new ZooKeeper(this.connectionString,500,this);
+        this.zk = new ZooKeeper(this.connectionString, 500, this);
     }
 
-    public void stop() throws InterruptedException{
+    public void stop() throws InterruptedException {
         zk.close();
         System.out.println("Exiting...");
     }
@@ -35,20 +50,28 @@ public class LeaderElection implements Watcher {
     private void read() throws InterruptedException {
         try {
             byte[] content = zk.getData("/leader", true, null);
-            System.out.println(serverId + ": " + new String(content));
-        } catch (KeeperException e) {
+            System.out.println(this.id + ": " + new String(content));
+        } catch (KeeperException.NoNodeException e) {
             // za slucaj da leader cvor nije napravljen ili je nestao
             join();
+        } catch (KeeperException e) {
+            e.printStackTrace();
         }
     }
-    public void join() throws InterruptedException{
-        try{
-            zk.create("/leader",serverId, ZooDefs.Ids.OPEN_ACL_UNSAFE,CreateMode.EPHEMERAL);
-            System.out.println("Uspesno sam postao leader");
+
+    public void join() throws InterruptedException {
+        try {
+            zk.create("/leader", getIdBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+            log("Uspesno sam postao lider");
             this.isLeader = true;
-        }catch (KeeperException e){
-            System.out.println("Leader vec postoji, nije moguce kreirati cvor");
+        } catch (KeeperException e) {
+            log("Leader vec postoji, nije moguce kreirati cvor");
             this.read();
         }
+    }
+
+    private void log(String message){
+        Calendar cal = Calendar.getInstance();
+        System.out.println("["+ this.id + "]"+ " " + "["+ dateFormat.format(cal.getTime()) + "]"+ " " + message);
     }
 }
--- src/domaci2/Main.java
+++ src/domaci2/Main.java
@@ -1,11 +1,17 @@
 package domaci2;
 
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooDefs;
+import org.apache.zookeeper.ZooKeeper;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Main {
-
+    static String connectionString = "localhost:2181";
     public static void main(String[] args) throws Exception {
         ch.qos.logback.classic.Logger root =
                 (ch.qos.logback.classic.Logger)
@@ -13,12 +19,39 @@ public class Main {
         root.setLevel(ch.qos.logback.classic.Level.OFF);
 
 
+        List<LeaderElection> leaderElections = new ArrayList<>();
 
-        LeaderElection le = new LeaderElection();
-        le.start();
-        le.join();
+        createEnvironment();
 
-        Thread.sleep(7000);
-        le.stop();
+        for (int i = 0; i < 4; i++) {
+            leaderElections.add(new LeaderElection(i,connectionString));
+        }
+        for (LeaderElection  le : leaderElections){
+            le.start();
+            le.join();
+        }
+        Thread.sleep(2000);
+
+        leaderElections.get(0).stop();
+        Thread.sleep(2000);
+        leaderElections.get(1).stop();
+        Thread.sleep(2000);
+        leaderElections.get(2).stop();
+        leaderElections.get(3).stop();
+    }
+    private static void createEnvironment() throws Exception {
+        ZooKeeper zk = new ZooKeeper(connectionString, 500, watchedEvent -> {});
+        try {
+            byte[] content = zk.getData("/votes",false, null);
+            System.out.println("ENV SETUP: Node /votes is available (content=["+new String(content)+"])");
+        } catch (KeeperException.NoNodeException e){
+            System.out.println("ENV SETUP: Node /votes not available, creating it...");
+            zk.create("/votes","voting".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+            System.out.println("ENV SETUP: Node /votes has been created successfully");
+        } catch (KeeperException e){
+            e.printStackTrace();
+        }
+        zk.close();
+
     }
 }
